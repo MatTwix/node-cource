5d8285f50369b32c6cfdde2af4a5b9e0f36718fa
MatTwix <mfgolden@yandex.ru>
2022-11-18 13:25:16 +0300

    repeat of hw1

--- colors_hw.js
+++ colors_hw.js
@@ -1,21 +1,21 @@
 // у меня проблема с цветами (мне сложно отличить желтый от зеленого), поэтому я буду использовать вместо зеленого цвета синий
-// также не совсем понял задание с интервалом, я так понимаю, должна вылетать ошибка, если разника между числами < 3
 const colors = require('colors/safe');
 
 const [num1, num2] = process.argv.splice(2);
-
+let currentNumber = 0;
+let arrOfNums = [];
 
 const getInterval = (a, b) => {
     if (isNaN(+a) || isNaN(+b)){
         console.log(colors.red('Введено не число'))
     } else if(a % 1 || b % 1) {
         console.log(colors.red('Введено некорректное число'))
-    } else if (b - a + 1 < 3){
-        console.log(colors.red('Интревал меньше 3-ех'))
     } else {
         let prevColor = 0;
         for(let i = a; i <= b; i++) {
-            prevColor === 0 ? console.log(colors.red(i)) : prevColor === 1 ? console.log(colors.yellow(i)) : console.log(colors.blue(i))
+            for(let n = 2; n < i; n++) arrOfNums.push(n);
+
+            if(arrOfNums.every(item => i % item)) prevColor === 0 ? console.log(colors.red(i)) : prevColor === 1 ? console.log(colors.yellow(i)) : console.log(colors.blue(i))
 
             prevColor+=1;
             prevColor=prevColor % 3;
